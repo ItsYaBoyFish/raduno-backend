5177bf743d3c384844ae8d7a55d7ea4c017f7822
Austin Graves <austingraves106@aol.com>
2020-01-17 21:08:04 -0600

    I changed the connection method for the DB

--- DB Help/connection.js
+++ DB Help/connection.js
@@ -1,7 +1,10 @@
 const { Sequelize } = require('sequelize');
 
 if (process.env.NODE_ENV="production") {
-  var db = new Sequelize(process.env.JAWSDB)
+  var db = new Sequelize('znr2as7eczh0w81j','z0xclpus1k3loekg','dth5gy96poigx66h',{
+    host: 'ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql'
+  })
 } else {
   var db = new Sequelize('radunoDB', 'active', 'password', {
     host: 'localhost',
